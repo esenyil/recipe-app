e7227f99df9524ec042f7ecd8f574b422445d484
Imaad <cph-ie47@cphbusiness.dk>
2022-05-05 11:49:23 +0200

    Forste step

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>velkommen til</h1>
-      <h1>Recipe app</h1>
+      
     </div>
   );
 }
 
-export default App;
+export default App
