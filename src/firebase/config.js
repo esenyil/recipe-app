d3008a5dbeeec8a8d829405cd6ad9a9bf31fdba0
esenyil <esenyildirim88@gmail.com>
2022-05-12 12:41:39 +0200

    firebase connect done

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firestore/firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCc1GIwuM9hGW9b-_0mGXbwBfa41JzKN98",
+    authDomain: "recipe-app-ee508.firebaseapp.com",
+    projectId: "recipe-app-ee508",
+    storageBucket: "recipe-app-ee508.appspot.com",
+    messagingSenderId: "401825885130",
+    appId: "1:401825885130:web:af1405e64e4e2892a77d33"
+  };
+
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
