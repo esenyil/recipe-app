3f8ac969ce93b8221ba04d98395c48b57ad22ca5
esenyil <esenyildirim88@gmail.com>
2022-05-12 20:56:25 +0200

    fetching data from firestore done

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,19 @@
-import firebase from 'firebase/app';
-import 'firestore/firebase';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCc1GIwuM9hGW9b-_0mGXbwBfa41JzKN98",
-    authDomain: "recipe-app-ee508.firebaseapp.com",
-    projectId: "recipe-app-ee508",
-    storageBucket: "recipe-app-ee508.appspot.com",
-    messagingSenderId: "401825885130",
-    appId: "1:401825885130:web:af1405e64e4e2892a77d33"
-  };
-
+  apiKey: "AIzaSyCc1GIwuM9hGW9b-_0mGXbwBfa41JzKN98",
+  authDomain: "recipe-app-ee508.firebaseapp.com",
+  projectId: "recipe-app-ee508",
+  storageBucket: "recipe-app-ee508.appspot.com",
+  messagingSenderId: "401825885130",
+  appId: "1:401825885130:web:af1405e64e4e2892a77d33",
+};
 
 // init firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 // init services
-const projectFirestore = firebase.firestore()
+const projectFirestore = firebase.firestore();
 
-export { projectFirestore }
+export { projectFirestore };
